ff5c73c6c6bcbfff4e4ee21606a25f5482ba5ec8
Gyane <162215993+Gyane001@users.noreply.github.com>
2026-03-27 13:39:54 -0300

    .
    .

---
+++ Assets/Scripts/Dialogue/GameData.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameData 
+{
+    // Armazena a quantidade de acertos
+    public static int totalWins = 0;
+
+    // Se você quiser saber EXATAMENTE quais questões ele acertou:
+    // public static List<string> questoesAcertadas = new List<string>();
+}
