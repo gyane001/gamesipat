34d714b1eecbc3c9b181a2075a22ca6de51e5a42
Gyane <162215993+Gyane001@users.noreply.github.com>
2026-04-06 14:19:50 -0300

    .
    .

--- Assets/Scripts/Dialogue/Pontuação/Resultados.cs
+++ Assets/Scripts/Dialogue/Pontuação/Resultados.cs
@@ -23,17 +23,17 @@ public class Resultados : MonoBehaviour
         if (acertos <= 0)
         {
             textoTitulo.text = "Perfil: Vulnerável";
-            textoDescricao.text = "Cuidado! Você costuma clicar em links suspeitos e não verifica a segurança das suas contas. Recomenda-se estudar mais sobre Phishing.";
+            textoDescricao.text = "Cuidado! Você não verifica a segurança do ambiente. Recomenda-se ser mais cauteloso.";
         }
         else if (acertos == 1)
         {
             textoTitulo.text = "Perfil: Cauteloso";
-            textoDescricao.text = "Bom trabalho! Você conhece o básico de segurança, mas ainda tem pontos cegos que podem ser explorados por hackers.";
+            textoDescricao.text = "Bom trabalho! Você conhece o básico de segurança, mas ainda tem pontos que podem ser explorados.";
         }
         else if ((acertos == 2))
         {
             textoTitulo.text = "Perfil: Especialista";
-            textoDescricao.text = "Parabéns! Você é um mestre da segurança digital. Suas escolhas protegem seus dados e sua privacidade online.";
+            textoDescricao.text = "Parabéns! Você é um mestre da segurança. Suas escolhas protegem sua vida e de seus colegas.";
         }
     }
 
