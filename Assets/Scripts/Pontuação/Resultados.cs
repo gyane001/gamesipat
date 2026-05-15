d64daa2652e62bea6d0b770412d45019ae86bf72
Gyane <162215993+Gyane001@users.noreply.github.com>
2026-05-15 09:13:53 -0300

    .
    .

--- Assets/Scripts/Pontuação/Resultados.cs
+++ Assets/Scripts/Pontuação/Resultados.cs
@@ -7,6 +7,7 @@ public class Resultados : MonoBehaviour
     [Header("Referências de UI")]
     [SerializeField] private TextMeshProUGUI textoTitulo;
     [SerializeField] private TextMeshProUGUI textoDescricao;
+    [SerializeField] private TextMeshProUGUI textoDesafio;
     [SerializeField] private TextMeshProUGUI textoContador;
 
     private void Start()
@@ -22,20 +23,25 @@ public class Resultados : MonoBehaviour
         // Lógica de filtragem de perfil
         if (acertos <= 3)
         {
-            textoTitulo.text = "Perfil: Vulnerável";
-            textoDescricao.text = "Você demonstrou que ainda existem lacunas importantes no seu conhecimento e nas suas atitudes relacionadas à segurança. Isso não significa falta de capacidade, mas sim uma oportunidade clara de aprendizado e mudança de comportamento.";
+            textoTitulo.text = "Perfil Ruim";
+            textoDescricao.text = "Ainda existem pontos importantes para desenvolver no conhecimento e nas atitudes de segurança. Reforce os conceitos, mantenha a atenção no dia a dia e transforme aprendizado em prática.";
+            textoDescricao.text += "\n\n\nDesafio: Aprenda com os erros e faça da segurança um hábito diário";
         }
+
         else if (acertos <= 8)
         {
-            textoTitulo.text = "Perfil: Cauteloso";
-            textoDescricao.text = "Você demonstrou um bom nível de conhecimento e consciência em segurança, aplicando corretamente muitos conceitos importantes no seu dia a dia. Suas escolhas indicam responsabilidade e atenção aos riscos..";
+            textoTitulo.text = "Perfil Bom";
+            textoDescricao.text = "Demonstrou bom conhecimento e consciência em segurança, com atitudes responsáveis no dia a dia. Continue atento aos detalhes, reforçando boas práticas e evoluindo continuamente.";
+            textoDescricao.text += "\n\n\nDesafio: Mantenha sua postura segura e seja exemplo para os colegas.";
         }
         else
         {
-            textoTitulo.text = "Perfil: Especialista";
-            textoDescricao.text = "Parabéns! Você é um mestre da segurança. Suas escolhas protegem você e seus colegas.";
+            textoTitulo.text = "Perfil Especialista";
+            textoDescricao.text = "Parabéns! Você acertou todas as respostas e demonstrou excelente conhecimento, responsabilidade e atitude em segurança. Suas escolhas mostram consciência dos riscos e compromisso com o cuidado próprio e coletivo.";
+            textoDescricao.text += "\n\nDesafio: Continue sendo exemplo e incentivando a cultura de segurança no dia a dia.";
         }
-        }
+    }
+
 
     public void ReiniciarJogo()
     {
